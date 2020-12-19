8cb4d5a173d931c4018dce293498b6860ac63351
GabrielaGonzaga <73176744+GabrielaGonzaga@users.noreply.github.com>
2020-12-19 18:53:17 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace _25_11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Clear();
+            Console.WriteLine("---------------------------------");
+            Console.WriteLine("---Sistema de passagens aéreas---");
+            Console.WriteLine("---------------------------------");
+
+
+            bool senhaValida = false;
+
+            do
+            {
+            Console.WriteLine("Digite sua senha");
+            string senha = Console.ReadLine();
+            senhaValida = EfetuarLogin(senha);
+            
+            } while (!senhaValida);
+            
+            
+
+            //Funções 
+            bool EfetuarLogin(string senha){
+                
+                if(senha == "123456"){
+                    return true;
+                }
+                else{
+                    Console.WriteLine("Senha incorreta");
+                    return false;
+                }
+            }
+
+            
+
+        }
+    }
+}
